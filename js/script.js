af8869c21166a8105caa694a7f0c74a30f0b2b1c
Vitor Eiken <vitoreiken@gmail.com>
2024-02-14 19:55:17 -0300

    fix: close the mobile menu when choosing the option

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-$(document).ready(function() {
-    $('#mobile-btn').on('click', function() {
-        $('#mobile-links').toggleClass('active')
-    })
-})
+const button = document.querySelector('#mobile-btn');
+const mobileLinks = document.querySelector('#mobile-links');
+
+const clickButton = () => {
+    const style = getComputedStyle(mobileLinks)
+    if(style.display === 'none') {
+        mobileLinks.style.display = 'flex'
+    } else {
+        mobileLinks.style.display = 'none'
+    }
+}
+
+const closeMenu = () => {
+    mobileLinks.style.display = 'none'
+}
+
+button.addEventListener('click', clickButton)
+mobileLinks.addEventListener('click', closeMenu)
